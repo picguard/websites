1a1b65030f6cccecc481fa5b222e0fc4bc0bfec4
kjxbyz <kjxbyz888@outlook.com>
2024-09-06 18:31:38 +0800

    fix: update locales

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -66,7 +66,7 @@ export default function Header(props: LngProps) {
                       </Link>
                     </TooltipTrigger>
                     <TooltipContent>
-                      <p>{t("menus.goto-picguard")}</p>
+                      <p>{t("menus.goto")}</p>
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
