fed14215239438a2d0f9dbd5fab47afa325de8c3
kjxbyz <kjxbyz888@outlook.com>
2024-12-04 19:23:40 +0800

    fix: update support email

--- app/[lng]/support/page.tsx
+++ app/[lng]/support/page.tsx
@@ -79,7 +79,7 @@ const apps = [
     demo: (
       <MdEmail className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
     ),
-    url: "mailto:support@insco.io",
+    url: "mailto:picguard-support@insco.io",
     large: false,
   },
   {
